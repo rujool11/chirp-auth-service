0df689001aac0bade9e3553c59aff0f42723c6fb
rujool11 <132924872+rujool11@users.noreply.github.com>
2025-10-15 20:59:03 +0530

    modify user schema

--- internal/db/createuser.go
+++ internal/db/createuser.go
@@ -12,14 +12,19 @@ func CreateUserTableIfDoesNotExist() {
 			username VARCHAR(50) UNIQUE NOT NULL,
 			email VARCHAR(50) UNIQUE NOT NULL,
 			password_hash VARCHAR(255) NOT NULL,
+			bio TEXT DEFAULT '',
+			likes_count INT DEFAULT 0,
+			followers_count INT DEFAULT 0,
+			following_count INT DEFAULT 0,
+			tweets_count INT DEFAULT 0,
 			created_at TIMESTAMP DEFAULT NOW()
-		); `
+		);
+	`
 
 	_, err := DB.Exec(context.Background(), query)
 	if err != nil {
 		log.Fatalf("SQL query error when creating table: %v", err)
 	}
 
-	log.Printf("Users table is ready")
-
+	log.Println("Users table is ready")
 }
