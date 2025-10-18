4dad8e0965f88cfbd548ff89c6510c56cc8da269
rujool11 <132924872+rujool11@users.noreply.github.com>
2025-10-18 15:18:14 +0530

    Add GetProfile controller

--- internal/controllers/profile.go
+++ internal/controllers/profile.go
@@ -1,8 +1,41 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/rujool11/chirp-auth-service/internal/db"
+	"github.com/rujool11/chirp-auth-service/internal/models"
+)
 
 func GetProfile(c *gin.Context) {
+	userIDVal, exists := c.Get("user_id")
+	if !exists {
+		c.JSON(401, gin.H{"error": "Unauthorized"})
+		return
+	}
+
+	userID := userIDVal.(int)
+	query := `SELECT id, username, email, bio, likes_count, followers_count,
+			following_count, tweets_count, created_at FROM users WHERE id=$1`
+
+	var user models.User
+	err := db.DB.QueryRow(c, query, userID).Scan(
+		&user.ID,
+		&user.Email,
+		&user.Username,
+		&user.Bio,
+		&user.LikesCount,
+		&user.FollowersCount,
+		&user.FollowingCount,
+		&user.TweetsCount,
+		&user.CreatedAt,
+	)
+
+	if err != nil {
+		c.JSON(400, gin.H{"error": "User not found"})
+		return
+	}
+
+	c.JSON(200, gin.H{"user": user})
 
 }
 
